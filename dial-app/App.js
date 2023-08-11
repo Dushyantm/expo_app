c2a031e838cfd6a74fd56dca9bb923b87bd68db3
Dushyant <dummahajan@gmail.com>
2023-08-11 15:17:41 -0400

    first_commit

---
+++ dial-app/App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { Link } from 'expo-router';
+
+
+export default function Page() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <Link href="/app/index">About</Link>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
